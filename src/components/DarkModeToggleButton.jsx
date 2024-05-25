fc5956884222e2941b47530338db3f7d7a461778
BrettRussell96 <148216281+BrettRussell96@users.noreply.github.com>
2024-05-25 11:57:11 +1000

    feat: footer style implemented

--- src/components/DarkModeToggleButton.jsx
+++ src/components/DarkModeToggleButton.jsx
@@ -1,15 +1,13 @@
 import { useDarkModeData, useDarkModeDispatch } from "../contexts/darkModeContext";
 
+export function DarkModeToggleButton(){
 
-export function DarkModeToggleButton() {
+	let darkModeData = useDarkModeData();
+	let darkModeDispatch = useDarkModeDispatch();
 
-    let darkModeData = useDarkModeData();
-    let darkModeDispatch = useDarkModeDispatch();
-
-    return(
-        <button onClick={() => darkModeDispatch(!darkModeData)}>
-            Toggle Dark Mode
-        </button>
-
-    )
+	return(
+		<button onClick={() => darkModeDispatch(!darkModeData)}>
+			Toggle Dark Mode
+		</button>
+	)
 }
